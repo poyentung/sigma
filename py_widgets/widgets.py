609036b635b3b6be57472e1796d767754cd67f36
Po-Yen Tung <t9918216@gmail.com>
2021-12-24 21:43:36 +0000

    Update widgets.py

--- py_widgets/widgets.py
+++ py_widgets/widgets.py
@@ -200,7 +200,7 @@ def show_unmixed_components(PC:PhaseClassifier, components:pd.DataFrame):
 
 def show_unmixed_weights_and_compoments(PC:PhaseClassifier, weights:pd.DataFrame, components:pd.DataFrame):
     # weights
-    weights.loc['sum'] = weights.sum()
+    weights.loc['mean'] = weights.mean()
     weights_options = weights.index
     multi_select_cluster = widgets.SelectMultiple(options=weights_options)
     plots_output = widgets.Output()
