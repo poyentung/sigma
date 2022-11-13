82ac1bee1de2f6ad9072975cc56a9f44d08c81fd
poyentung <t9918216@gmail.com>
2022-11-13 17:47:22 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
-__version__ = "0.1.30"
+__version__ = "0.1.31"
     
 setup(
     name='emsigma',
